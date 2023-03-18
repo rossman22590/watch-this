cfbc35527fa820fa12b2c1b826e23088dc32f821
Ross Cohen <rosscohen1990@gmail.com>
2023-03-18 12:49:47 -0400

    Update GitHubButton.svelte

--- src/lib/GitHubButton.svelte
+++ src/lib/GitHubButton.svelte
@@ -1,4 +1,4 @@
-<div>
+<!-- <div>
 	<a
 		class="my-1 flex flex-none items-center py-1 px-4 border border-white/50 rounded-full text-sm bg-white/20 hover:bg-white/50 shadow-sm hover:text-slate-900 text-white"
 		href=""
@@ -18,3 +18,4 @@
 		<span class="ml-2">Star on GitHub</span>
 	</a>
 </div>
+ -->
