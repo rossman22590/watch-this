b3e8ee9b43491f7aae99de6934996e673a2d4e38
Ross Cohen <rosscohen1990@gmail.com>
2023-03-18 12:43:12 -0400

    Update Footer.svelte

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,6 +1,6 @@
-<script>
+<!-- <script>
 	import GitHubButton from './GitHubButton.svelte';
-</script>
+</script> -->
 
 <div
 	class="max-w-4xl mx-auto w-full flex md:flex-row flex-col items-center md:justify-between py-8 mt-20 border-t border-white/20"
