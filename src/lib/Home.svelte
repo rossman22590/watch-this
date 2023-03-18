4b9b6ca3cd568db8d620739454bab7e02bd4c8c9
Ross Cohen <rosscohen1990@gmail.com>
2023-03-18 12:42:53 -0400

    Update Home.svelte

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -12,12 +12,12 @@
 		>
 			Get recommendations
 		</button>
-		<a
+<!-- 		<a
 			href="https://www.youtube.com/watch?v=Z3LrfdctOpU"
 			target="_blank"
 			class="w-full md:w-auto text-center bg-white/20 hover:bg-white/50 mb-4 flex-none rounded-full text-white font-medium py-3 px-6"
 		>
 			Learn how it's built
-		</a>
+		</a> -->
 	</div>
 </div>
